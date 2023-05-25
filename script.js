43a37b4cc2832b78506dc2964f4adde2116ac486
Dikesh01 <117905520+Dikesh01@users.noreply.github.com>
2023-05-25 18:48:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const inputs = document.getElementByTagName("input")
-const input1 = inputs[0].innerText;
-const input2 = inputs[1].innerText;
+const inputs = document.getElementsByTagName("input")
+const input1 = inputs[0];
+const input2 = inputs[1];
 
 
 function getFormvalue() {
     //Write your code here
-	alert(input1 input2)
-
+	alert(input1.value + " " + input2.value)
 }
